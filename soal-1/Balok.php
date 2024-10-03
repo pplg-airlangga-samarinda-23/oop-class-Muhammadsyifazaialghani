a95cff3ed79085b3826469e62740379e4cd7d456
Muhammadsyifazaialghani <23_muhammadsyifazai@student.smkti.net>
2024-10-03 12:51:44 +0800

    selesai

--- soal-1/Balok.php
+++ soal-1/Balok.php
@@ -2,23 +2,65 @@
 class Balok
 {
     // deklarasikan atribut-atribut kelas Balok
-
-    
+    public $panjang;
+    public $lebar;
+    public $tinggi;
     // definisikan metode konstruktor, setter ,dan getter
+    public function __construct($panjang, $lebar, $tinggi)
+    {
+        $this->panjang = $panjang;
+        $this->lebar = $lebar;
+        $this->tinggi = $tinggi;
+    }
 
-
+    public function setPanjang()
+    {
+        return $this->panjang;
+    }
+    public function setLebar()
+    {
+        return $this->lebar;
+    }
+    public function setTinggi()
+    {
+        return $this->tinggi;
+    }
+    public function getPanjang() {
+        return $this->panjang;
+    }
+    public function getLebar() {
+        return $this->lebar;
+    }
+    public function getTinggi() {
+        return $this->tinggi;
+    }
+    
     // definisikan metode menghitung volume, luas permukaan, 
+    public function hitungVolume()
+    {
+        return ($this->panjang * $this->lebar * $this->$tinggi);
+    }
+    public function hitungLuasPermukaan()
+    {
+        return 2*($this->panjang + $this->lebar + $this->$tinggi);
+    }
 }
 
 $balok_1 = new Balok(4, 8, 5);
 $balok_2 = new Balok(3, 10, 12);
-
 // contoh output: Balok
+echo "Balok 1 <br>";
 
 // Balok 1
 // Volume: 160
 // Luas Permukaan: 184
-
+echo "balok:".$balok_1->panjang . "<br>";
+echo "Volume:".$balok_1->lebar . "<br>";
+echo "Luas Permukaan:".$balok_1->tinggi . "<br> <br>";
 // Balok 2
 // Volume: 360
 // Luas Permukaan: 372
+echo "Balok 2 <br>";
+echo "balok:".$balok_2->panjang . "<br>";
+echo "Volume:".$balok_2->lebar . "<br>";
+echo "Luas Permukaan:".$balok_2->tinggi . "<br>";
--- soal-2/Siswa.php
+++ soal-2/Siswa.php
@@ -3,22 +3,78 @@
 class Siswa
 {
     // deklarasikan atribut-atribut kelas Siswa
+    public $nama;
+    public $nisn;
+    public $alamat;
+    public $jurusan;
+    public $tanggallahir;
 
-    
     // definisikan metode konstruktor, setter ,dan getter
-
+    public function __construct($nama, $nisn, $alamat, $jurusan, $tanggallahir)
+    {
+        $this->nama = $nama;
+        $this->nisn = $nisn;
+        $this->alamat = $alamat;
+        $this->jurusan = $jurusan;
+        $this->tanggallahir = $tanggallahir;
+    }
+    public function setNama()
+    {
+        return $this->nama;
+    }
+    public function setNisn()
+    {
+        return $this->nisn;
+    }
+    public function setAlamat()
+    {
+        return $this->alamat;
+    }
+    public function setJurusan()
+    {
+        return $this->jurusan;
+    }
+    public function setTanggallahir()
+    {
+        return $this->tanggallahir;
+    }
+    public function getNama() {
+        return $this->nama;
+    }
+    public function getNisn() {
+        return $this->nisn;
+    }
+    public function getAlamat() {
+        return $this->alamat;
+    }
+    public function getJurusan() {
+        return $this->jurusan;
+    }
+    public function getTanggallahir() {
+        return $this->tanggallahir;
+    }
+    
 
     // buat metode untuk menghitung umur siswa
-    public function hitungUmur() {
+    public function hitungUmur($umur) {
 
-
-        return $umur;
+        return $this->$umur * $this->tanggallahir;
     }
 }
 
 // buat dua buah objek siswa
 $siswa_1 = new Siswa("Bambang", "1234", "Loa Bakung", "PPLG", "27-11-2006");
 $siswa_2 = new Siswa("Bahrun", "1235", "Air Putih", "PPLG", "13-01-2007");
-
+echo "Nama: ". $siswa_1->nama . "<br>";
+echo "Nisn: ". $siswa_1->nisn . "<br>";
+echo "Alamat: ". $siswa_1->alamat . "<br>";
+echo "Jurusan: ". $siswa_1->jurusan . "<br>";
+echo "Tanggal Lahir: ". $siswa_1->tanggallahir . "<br> <br>";
+echo "Nama: ". $siswa_2->nama . "<br>";
+echo "Nisn: ". $siswa_2->nisn . "<br>";
+echo "Alamat: ". $siswa_2->alamat . "<br>";
+echo "Jurusan: ". $siswa_2->jurusan . "<br>";
+echo "Tanggal Lahir: ". $siswa_2->tanggallahir . "<br> <br>";
+echo "Bambang Lebih Tua dibandingan Bahrun.";
 // contoh output:
 // Bambang lebih tua dibandingkan Bahrun.
